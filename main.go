58dd862404cdbcce9c94472b4d939ff39004f67d
Brad Rydzewski <brad.rydzewski@gmail.com>
2015-11-23 16:33:48 -0800

    added system to hook so we have the drone url

--- main.go
+++ main.go
@@ -13,20 +13,24 @@ import (
 func main() {
 	var repo = drone.Repo{}
 	var build = drone.Build{}
+	var sys = drone.System{}
 	var vargs = struct {
 		Webhook drone.StringSlice `json:"webhook"`
 	}{}
 
 	plugin.Param("repo", &repo)
 	plugin.Param("build", &build)
+	plugin.Param("system", &sys)
 	plugin.Param("vargs", &vargs)
 	plugin.Parse()
 
 	// data structure
+	// old formats https://github.com/gitterHQ/services/blob/master/lib/drone/examples/enterprise_github_commit_success.json
 	data := struct {
-		Repo  drone.Repo  `json:"repo"`
-		Build drone.Build `json:"build"`
-	}{repo, build}
+		Repo  drone.Repo    `json:"repo"`
+		Build drone.Build   `json:"build"`
+		System drone.System `json:"system"`
+	}{repo, build, sys}
 
 	// json payload that will be posted
 	payload, err := json.Marshal(&data)
